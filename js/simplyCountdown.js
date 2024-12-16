720e95b77ddd9581682c2af55741a5092943bad8
year end party <30817257+kimthuyet@users.noreply.github.com>
2024-12-16 14:47:44 +0700

    Update simplyCountdown.js

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -115,7 +115,7 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2024,
+                year: 2025,
                 month: 5,
                 day: 18,
                 hours: 0,
